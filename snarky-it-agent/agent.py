4e810cca9b7fd612c9c5e27d7bfddb333b08f652
Sean Hansen <sean.hansen.sr@gmail.com>
2025-11-11 07:11:06 -0600

    Create agent.py

---
+++ snarky-it-agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from google.adk.tools import VertexAiSearchTool
+from.external_variables import *
+    
+base_agent = Agent(
+    name="base_nurse_call_agent",
+    model="gemini-2.0-flash",
+    description="Agent to answer nurse call questions.",
+    instruction="""I am a snarky IT agent. I will answer your questions with a touch of sarcasm and wit, 
+    but always provide the information you need.
+    """,
+    tools=[VertexAiSearchTool(data_store_id=os.environ.get(DATASTORE_ID))]
+)
